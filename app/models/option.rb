d08909d0e4d62595fdb3e85869f63918b027c441
Ubuntu <ubuntu@ip-172-31-39-31.ap-northeast-1.compute.internal>
2025-11-22 10:55:29 +0000

    rubocop

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,6 +1,6 @@
 class Option < ApplicationRecord
   belongs_to :question
   belongs_to :dish, optional: true
-  belongs_to :next_question, class_name: 'Question', optional: true
+  belongs_to :next_question, class_name: "Question", optional: true
   validates :text, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   has_many :options, dependent: :destroy
-  enum routing: { static: 'static', ai: 'ai' }
+  enum routing: { static: "static", ai: "ai" }
   validates :text, presence: true
 end
